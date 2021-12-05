10a5f5371ae1934930f7a88b189ef8fbd2963ebc
Brandon Hoskins <branjames117@gmail.com>
2021-12-04 23:50:02 -0600

    generating table of contents with a separate function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,57 +1,100 @@
 const generateLicenseLink = require('./generateLicenseLink.js');
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `[](https://img.shields.io/badge/License-${license}-blue)`;
+}
 
 function renderContributingSection(contributing) {
   return `## Contributing
 
   ${contributing}
-  `;
+`;
 }
 
 function renderTestsSection(tests) {
   return `## Tests
 
   ${tests}
-  `;
+`;
+}
+
+function renderQuestionsSection(email) {
+  return `## Questions
+  
+  With any questions email the repository owner at [${email}](mailto:${email})
+`;
+}
+
+function renderTableOfContentsSection(data) {
+  let basicMarkdown = `* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)`;
+
+  if (data.contributing) {
+    basicMarkdown += `
+* [Contributing](#contributing)`;
+  }
+
+  if (data.tests) {
+    basicMarkdown += `
+* [Tests](#tests)`;
+  }
+
+  if (data.email) {
+    basicMarkdown += `
+* [Questions](#questions)`;
+  }
+
+  return basicMarkdown;
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   console.log(data);
-  return `# ${data.title}
+
+  // destructure the data object
+  const {
+    title,
+    username,
+    email,
+    description,
+    installation,
+    usage,
+    license,
+    tests,
+    contributing,
+  } = data;
+
+  // get license text, link, and badge URL
+  const licenseMarkdown = generateLicenseLink(license, username);
+  const tableOfContentsMarkdown = renderTableOfContentsSection(data);
+
+  return `# ${title}
 
 ## Description
+${licenseMarkdown.badge}
 
-${data.description}
+${description}
 
 ## Table of Contents
 
-* [Installation](#installation)
-* [Usage](#usage)
-* [License](#license)
-${data.contributing ? '* [Contributing](#contributing)' : ''}
-${data.tests ? '* [Tests](#tests)' : ''}
-${data.email ? '* [Questions](#questions)' : ''}
+${tableOfContentsMarkdown}
 
 ## Installation
 
-${data.installation}
+${installation}
 
 ## Usage
 
-${data.usage}
+${usage}
 
 ## License
 
-${generateLicenseLink(data.license, data.username)}
-
-${data.contributing ? renderContributingSection(data.contributing) : ''}
-
-${data.tests ? renderTestsSection(data.tests) : ''}
-
-`;
+${licenseMarkdown.text}
+${contributing ? renderContributingSection(contributing) : ''}
+${tests ? renderTestsSection(tests) : ''}
+${email ? renderQuestionsSection(email) : ''}`;
 }
 
 module.exports = generateMarkdown;
